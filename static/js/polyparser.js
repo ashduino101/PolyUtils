16e959d9754cc8d4540bac091b1d22b7573a567d
Ashton Fairchild <67023707+ashduino101@users.noreply.github.com>
2022-09-15 17:17:59 -0700

    v27/v28 support

--- static/js/polyparser.js
+++ static/js/polyparser.js
@@ -22,7 +22,7 @@
  * DEALINGS IN THE SOFTWARE.
  */
 
-const MAX_LAYOUT_VERSION = 26;
+const MAX_LAYOUT_VERSION = 28;
 const MAX_BRIDGE_VERSION = 11;
 const MAX_SLOT_VERSION = 3;
 const MAX_PHYSICS_VERSION = 1;
@@ -655,6 +655,7 @@ class Deserializer {
             if (version >= 6) {
                 island.lockPosition = this.readBool();
             }
+            island.hidden = (this.layout.version >= 27 && this.readBool());
 
             this.layout.terrainStretches.push(island);
         }
@@ -819,6 +820,7 @@ class Deserializer {
         // Settings
         this.layout.settings.hydraulicsControllerEnabled = this.readBool();
         this.layout.settings.unbreakable = this.readBool();
+        this.layout.settings.no_water = (this.layout.version >= 28 && this.readBool());
 
         // Custom shapes in v9+
         if (version > 9) {
@@ -1213,7 +1215,7 @@ class Serializer {
             this.writeString(e.nodeBGuid);
             this.writeInt32(e.jointAPart);
             this.writeInt32(e.jointBPart);
-            this.writeString(e.guid ? e.guid : createUUID())
+            this.writeString(e.guid ? e.guid : createUUID());
         }
         // Springs
         this.writeInt32(b.springs.length);
@@ -1367,6 +1369,7 @@ class Serializer {
             this.writeInt32(s.variantIndex);
             this.writeBool(s.flipped);
             this.writeBool(s.lockPosition);
+            this.writeBool(s.hidden);
         }
 
         // Platforms
@@ -1464,6 +1467,7 @@ class Serializer {
         // Settings
         this.writeBool(this.layout.settings.hydraulicsControllerEnabled); // Hydraulics controller enabled
         this.writeBool(this.layout.settings.unbreakable); // Unbreakable
+        this.writeBool(this.layout.settings.no_water); // No water
 
         // Custom shapes
         this.writeInt32(this.layout.customShapes.length);
@@ -1623,19 +1627,19 @@ class SlotDeserializer {
         return view.getBigUint64(0)
     }
     readString() {
-        // UTF-8 or UTF-16LE
+        // I don't know how this works, I just ported it from the original code
         let num = this.readByte();
         if (num < 0) {
             return "";
         }
-        if (num === 0x00) {  // utf8
+        if (num === 0x00) {
             let num2 = this.readInt32();
             return new TextDecoder().decode(this.readBytes(num2));
         }
-        if (num === 0x01) {  // utf16
+        if (num === 0x01) {
             let num3 = this.readInt32();
             let num4 = num3 * 2;
-            return new TextDecoder('utf-16').decode(this.readBytes(num4));
+            return new TextDecoder().decode(this.readBytes(num4));
         }
     }
     assert(condition, message) {
